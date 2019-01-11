6903a178883a7e909f5a9306f4ccd6ad1b12e6fe
Mao-Chun Lin <44393714+ADaei@users.noreply.github.com>
2019-01-11 14:04:18 +0800

    Create install_vim.sh

---
+++ vim/install_vim.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## intall vim ##
+sudo apt-get install vim
+
+## intall cscope ##
+sudo apt-get install cscope
+
+## intall ctags ##
+sudo apt-get install ctags
+
+## is this enough?? ##
